2c1e3c0c18ffa6d85b6ab37ccc95d104447bbde0
Kenaniah Cerny <kenaniah@gmail.com>
2021-04-13 22:18:32 -0700

    checkpoint

---
+++ tests/subscriptions_test.rs
@@ -0,0 +1,55 @@
+mod setup;
+
+use rocket::http::uri::{Query, UriDisplay};
+use rocket::UriDisplayQuery;
+
+#[derive(UriDisplayQuery)]
+struct Body<'a> {
+    name: Option<&'a str>,
+    email: Option<&'a str>,
+}
+
+impl Body<'_> {
+    /// Returns the struct as an URI-formatted string
+    pub fn to_uri(&self) -> String {
+        format!("{}", &self as &dyn UriDisplay<Query>)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rocket::http::{ContentType, Status};
+
+    #[test]
+    fn request_body() {
+        let body = Body {
+            name: Some("Some One"),
+            email: Some("foo@example.com"),
+        };
+        assert_eq!(body.to_uri(), "name=Some%20One&email=foo@example.com");
+        let body = Body {
+            name: None,
+            email: None,
+        };
+        assert_eq!(body.to_uri(), "");
+    }
+
+    #[test]
+    fn subscribe_returns_a_200_for_valid_form_data() {
+        let client = super::setup::blocking_client();
+        let response = client
+            .post("/subscriptions")
+            .header(ContentType::Form)
+            .body(
+                Body {
+                    name: Some("Some Person"),
+                    email: Some("someone@example.com"),
+                }
+                .to_uri(),
+            )
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string(), None);
+    }
+}
