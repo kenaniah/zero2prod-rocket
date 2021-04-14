2de53476bf02136a6e91bc2e8477c94699a32ac7
Kenaniah Cerny <kenaniah@gmail.com>
2021-04-13 19:23:15 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,20 @@ fn health_check() -> Status {
 fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/", routes![health_check])
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn health_check() {
+        let rocket = super::rocket();
+        let client = Client::tracked(rocket).expect("valid rocket instance");
+        let response = client.get("/health_check").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string(), None);
+    }
+
+}
