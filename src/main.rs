5977dcc02e952a13c4bb5d54164bd13a3f38e26f
Kenaniah Cerny <kenaniah@gmail.com>
2021-04-13 19:04:15 -0700

    health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::Status;
+
+#[get("/health_check")]
+fn health_check() -> Status {
+    Status::Ok
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![health_check])
 }
